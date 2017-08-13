152020bfd882181f752b90b088e14fb9538116fb
ram pradeep <30342988+Rampradeeprs@users.noreply.github.com>
2017-08-13 21:35:39 +0530

    Create sumint.c

---
+++ sumint.c
@@ -0,0 +1,31 @@
+#injclude<stdio.h>
+void main()
+{
+int n,i,sum,odd_sum=0;
+printf("enter the number");
+scanf("%d",&n);
+case 1:
+{
+for(i=1;i<=15;i++)
+{
+sum+=i;
+}
+printf("the sum of the number is=5d");
+break;
+}
+case2:
+for(i=15;1<=45;i++)
+{
+if(i%2!=0)
+{
+odd_sum=odd_sum+i;
+printf("sum of odd_sum=%d");
+}
+break;
+}
+}
+
+
+
+
+
